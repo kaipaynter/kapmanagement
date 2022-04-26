01ee8f8945338014f29fbb09ea9f182db2211a5b
beepa <admin@geartigerproductions.com>
2022-04-26 14:55:57 +0200

    first release

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,10 +5,34 @@ import Layout from '../components/Layout';
 const ContactPage = () => (
   <Layout fullMenu>
     <article id="main">
-    <header>
+      <header>
         <h2>Contact</h2>
       </header>
     </article>
+    <section className="main">
+      <form method="post" action="https://formspree.io/f/mvodweje">
+        <label>
+          Name
+          <input type="text" name="name" id="name" />
+        </label>
+        <label>
+          Email
+          <input type="email" name="_replyto" id="email" />
+        </label>
+        <label>
+          Subject
+          <input type="text" name="subject" id="subject" />
+        </label>
+        <label>
+          Message
+          <textarea name="message" id="message" rows="5" />
+        </label>
+        <button type="submit" className="button special">
+          Send
+        </button>
+        <input type="reset" value="Clear" />
+      </form>
+    </section>
   </Layout>
 );
 
