4d1315b83d71771fc1d0ff62ecd8a99a2656aaf4
beepa <admin@geartigerproductions.com>
2022-04-26 13:25:55 +0200

    config changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,10 @@ import Layout from '../components/Layout';
 const AboutPage = () => (
   <Layout fullMenu>
     <article id="main">
-      
+      <header>
+        <h2>About</h2>
+        <p>Aliquam ut ex ut interdum donec amet imperdiet eleifend</p>
+      </header>
     </article>
   </Layout>
 );
