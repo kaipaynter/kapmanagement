d857faf314b738a8368697f0b1cf3129cac15445
boipelomawasha-pers <boipelomawasha@gmail.com>
2022-05-11 09:22:03 +0200

    latest updates

--- src/pages/submissions.js
+++ src/pages/submissions.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/Layout';
 
@@ -8,9 +9,14 @@ const SubmissionsPage = () => (
       <header>
         <h2>Unsolicited Submissions Policy</h2>
       </header>
-      <p>
-        KAP Management has a policy that neither it nor any of its agents or other employees shall accept or consider any unsolicited material, ideas or suggestions of any nature whatsoever (“Unsolicited Materials”). Accordingly, you may not use this website or information obtained there from to submit Unsolicited Materials to KAP Management via any means (including, without limitation, via mail, fax or e-mail). Should you nevertheless contravene this express prohibition by sending Unsolicited Materials to KAP Management, please be advised that the Unsolicited Materials will not be considered by anyone at KAP Management, and if possible they will be returned to you with no copies kept. Unsolicited Materials will not be forwarded to or discussed with any third parties.
-      </p>
+      <span className="image fit">
+        <StaticImage
+          src="../assets/images/Submissions_Policy.jpeg"
+          alt="KAP Management"
+          placeholder="blurred"
+          objectFit="contain"
+        />
+      </span>
     </article>
   </Layout>
 );
