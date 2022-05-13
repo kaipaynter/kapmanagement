cedf6f2ac13e36e607dcd5fcc4db8fa3cc732e83
boipelomawasha-pers <boipelomawasha@gmail.com>
2022-05-13 23:11:55 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c0572487198253478c44.js"
+    "url": "webpack-runtime-58eaf64138d02ec76555.js"
   },
   {
     "url": "framework-e43d5422692f7937c273.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "120fb15050548b4d2a77028e52554d63"
+    "revision": "a8c6c0baffb1284da54f1f0001b9ccfa"
   },
   {
     "url": "polyfill-981201e99182758cb9c8.js"
